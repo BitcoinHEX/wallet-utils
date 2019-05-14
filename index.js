7262b0a8c38a54e08b3ddbe136f49fd7ded63e3f
arc@second.com <arc@second.com>
2019-05-14 15:08:14 +0800

    Framework

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const utils = {}
+const utils = {};
+
+utils.add = function (a, b) {
+  return a + b;
+};
 
 module.exports = utils;
--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
+const assert = require('assert');
 const hexUtils = require('../index');
+
+describe('hexUtils', () => {
+  describe('add()', () => {
+    it('should add the numbers together', () => {
+      assert.equal(hexUtils.add(1, 2), 3);
+    });
+  });
+});
