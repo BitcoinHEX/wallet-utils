0502d82349c2da8508d69b3bbe7c2834ccd776d6
arc@second.com <arc@second.com>
2019-05-16 09:28:14 +0800

    Day proxy not needed

--- test/transform.js
+++ test/transform.js
@@ -2,17 +2,4 @@ const assert = require('assert');
 const hexUtils = require('../index');
 
 describe('transform', () => {
-  describe('dayProxy()', () => {
-    it('Should remap day -1 to 352', () => {
-      assert.equal(hexUtils.transform.dayProxy(-1), 352);
-    });
-    it('Should remap day -2 to 351', () => {
-      assert.equal(hexUtils.transform.dayProxy(-2), 351);
-    });
-    it('Should return expected day for all other days', () => {
-      assert.equal(hexUtils.transform.dayProxy(1), 1);
-      assert.equal(hexUtils.transform.dayProxy(15), 15);
-      assert.equal(hexUtils.transform.dayProxy(68), 68);
-    });
-  });
 });
--- transform/index.js
+++ transform/index.js
@@ -1,10 +1,4 @@
 const transform = {
-  dayProxy(day) {
-    let dayProxy = day;
-    if (day === -2) dayProxy = 351;
-    if (day === -1) dayProxy = 352;
-    return dayProxy;
-  },
 };
 
 module.exports = transform;
